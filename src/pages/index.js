dcd0db0e5669f945c1811aeaa6782fbff008fce9
64070tmkst <55692778+64070tmkst@users.noreply.github.com>
2020-04-16 01:04:45 +0900

    the first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>
+      Hi! I'm Tomoki Sato.<br/>
+      I'm a UI/UX Designer.
+    </h1>
+    <p>Welcome to my portfolio site!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2!</Link>
   </Layout>
 )
 
