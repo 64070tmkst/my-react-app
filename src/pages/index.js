2e3df44cd28573dbaf627eba0d1985da6144ee05
64070tmkst <55692778+64070tmkst@users.noreply.github.com>
2020-04-16 01:08:56 +0900

    I changed the link text

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2!</Link>
+    <Link to="/page-2/">Click here to go to the next page</Link>
   </Layout>
 )
 
